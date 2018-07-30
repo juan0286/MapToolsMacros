908719f3627adb635ad83df9850de79237d7125b
Juan osudar <juan.0286@gmail.com>
2018-07-30 12:12:39 -0300

    Creo Selector de Accion html

---
+++ Rol Tactico/Campaign/SelAccion.js
@@ -0,0 +1,98 @@
+[ frame("SelAccion"):{
+ <html>
+    <head>
+      <title>Acciones en el Asalto</title>
+    </head>
+    <body>
+	<table width='100%' cellpadding='0' cellspacing='0'>
+		 <html>
+    <head>
+      <title>Botonera de Guerra</title>
+    </head>
+    <body>
+	<table width='100%' cellpadding='0' cellspacing='3'>
+		<tr>
+			<td style='padding-top: 12px;
+			    padding-bottom: 12px;
+			    text-align: center;
+			    background-color: #4CAF50;
+			    color: white;'>
+				<a href='macro://guardarAccion@Lib:asaltos/self/selected?cargar_sort'>
+					<span style='text-decoration:none; color:blue;'>ACTION</span></a>
+			</td>
+		</tr>
+		<tr>
+			<td style='padding-top: 12px;
+			    padding-bottom: 12px;
+			       text-align: center;
+			    background-color: #4CAF50;
+			    color: white;'>
+				<a href='macro://guardarAccion@Lib:asaltos/self/selected?lanzar_sort'>
+					<span style='text-decoration:none; color:blue;'>ACTION</span></a>
+			</td>
+		</tr>
+		<tr>
+			<td style='padding-top: 12px;
+			    padding-bottom: 12px;
+			       text-align: center;
+			    background-color: #4CAF50;
+			    color: white;'>
+				<a href='macro://guardarAccion@Lib:asaltos/self/selected?cargar_proy'>
+					<span style='text-decoration:none; color:blue;'>ACTION</span></a>
+			</td>
+		</tr>
+		<tr>
+			<td style='padding-top: 12px;
+			    padding-bottom: 12px;
+			       text-align: center;
+			    background-color: #4CAF50;
+			    color: white;'>
+				<a href='macro://guardarAccion@Lib:asaltos/self/selected?disparar_proy'>
+					<span style='text-decoration:none; color:blue;'>ACTION</span></a>
+			</td>
+		</tr>
+		<tr>
+			<td style='padding-top: 12px;
+			    padding-bottom: 12px;
+			       text-align: center;
+			    background-color: #4CAF50;
+			    color: white;'>
+				<a href='macro://guardarAccion@Lib:asaltos/self/selected?mov_manio'>
+					<span style='text-decoration:none; color:blue;'>ACTION</span></a>
+			</td>
+		</tr>
+		<tr>
+			<td style='padding-top: 12px;
+			    padding-bottom: 12px;
+			       text-align: center;
+			    background-color: #4CAF50;
+			    color: white;'>
+				<a href='macro://guardarAccion@Lib:asaltos/self/selected?ataque_cac'>
+					<span style='text-decoration:none; color:blue;'>ACTION</span></a>
+			</td>
+		</tr>
+		<tr>
+			<td style='padding-top: 12px;
+			    padding-bottom: 12px;
+			       text-align: center;
+			    background-color: #4CAF50;
+			    color: white;'>
+				<a href='macro://guardarAccion@Lib:asaltos/self/selected?desplazamiento'>
+					<span style='text-decoration:none; color:blue;'>ACTION</span></a>
+			</td>
+		</tr>
+		<tr>
+			<td style='padding-top: 12px;
+			    padding-bottom: 12px;
+			       text-align: center;
+			    background-color: #4CAF50;
+			    color: white;'>
+				<a href='macro://guardarAccion@Lib:asaltos/self/selected?mov_estatico'>
+					<span style='text-decoration:none; color:blue;'>ACTION</span></a>
+			</td>
+		</tr>
+</table>
+</body>
+</html>
+
+}]
