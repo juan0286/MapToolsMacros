51a331f15c0335bac883a613a9fb1bd58a61e5f4
Juan osudar <juan.0286@gmail.com>
2018-07-29 22:28:10 -0300

    Movimiento Rapido
    Esta version aùn tiene ActualMove, futuramente  inservible

--- Rol Tactico/OntokenMove_tok/MoverToken.js
+++ Rol Tactico/OntokenMove_tok/MoverToken.js
@@ -1,21 +1,26 @@
-[h: isSufiscientesPuntos = arg(0)]	
-[h: token = arg(1)]	
-[h: usedMov = arg(2)]	
-[h: mov = getProperty("Movement", token) - usedMov]	
-[h: mje = token+ " se movio "+ usedMov +". Aun se puede mover "+ mov ]			
+[h: token = arg(0)]	
+[h: usedMov = arg(1)]
 
-[h, if( isSufiscientesPuntos ),CODE:{   	
-		
-		[h: setProperty("ActualMove", mov,token)]		
-		[r, if( isPC(token) ), CODE:	{								
-			<!-- Si Tiene seguimiento lo mueve -->
-			[r, if( isPropertyEmpty("Seguimiento",token) == 0): Seguir(token,getLastPath(0)) ]
+[h: rango = 1]	
+[h: mov = getProperty("ActualMove", token)]	
 
-		}]
-		
-	};{		
-		[h, if( isPC(token) ), CODE:	{			
-			[h: mje = '<span style="color:red;font-weight:bold;">'+ token +' solo puede moverse '+ getProperty("ActualMove")+ '.</span>' ]			
-	}]	
-}]	
+[h, if(usedMov <=  mov): rango =  1]
+[h, if(usedMov >  mov && usedMov <=  mov*1.5 ): rango =  2]
+[h, if(usedMov >  mov*1.5 && usedMov <=  mov*2 ): rango =  3]
+[h, if(usedMov >  mov*2): rango =  4]
+
+[h: am = getProperty("ActualMove", token)]
+
+[h, if( rango == 1 ): mov_final = getProperty("ActualMove", token) - usedMov ; mov_final = 0 ]
+[h: mje = token+ " se movio "+ usedMov +". Aun se puede mover "+ mov_final ]	
+
+[h, if( rango < 4 ),CODE:{   			
+	[h: setProperty("ActualMove", mov_final,token)]		
+	[h: setProperty("LastRoute", usedMov,token)]	
+	<!-- Si Tiene seguimiento lo mueve -->			
+};{
+	[h: mje = '<span style="color:red;font-weight:bold;">'+ token +' no puede moverse tanto en este turno.</span>' ]					
+}]
+[h: DialogRango(rango,getProperty("Movement"))  ]
+
 [h: macro.return = mje]
--- Rol Tactico/OntokenMove_tok/OnTokenMove.js
+++ Rol Tactico/OntokenMove_tok/OnTokenMove.js
@@ -10,13 +10,21 @@
 [h, if( id == getInitiativeToken() ): isInictiavaOken =  1; isInictiavaOken =  0]
 [h: isEnGrilla=isSnapToGrid()]
 [h, if( isEnGrilla ):usedMov =  getMoveCount()]
-
 [h, if(usedMov <=  mov): isSufiscientesPuntos =  1; isSufiscientesPuntos =  0]
+[h, if(usedMov >  mov && usedMov <=  mov*1.5 ): isRango =  1; isRango =  0]
+[h, if(usedMov >  mov*1.5 && usedMov <=  mov*2.5 ): isRango3 =  1; isRango3 =  0]
+[h, if(usedMov >  mov*2.5): isRango4 =  1; isRango4 =  0]
 
 
-[h,if( isSufiscientesPuntos && isInictiavaOken ): mje = MoverToken(isSufiscientesPuntos,name,usedMov); isNoMover=1]
 
-[h, if( isInictiavaOken == 0 ): mje= '<span style="coloh:red;font-weight:bold;">'+ name +' Debe esperar su turno.</span>']
+[h, if( isInictiavaOken ): 
+mje = MoverToken(isSufiscientesPuntos,name,usedMov) ; 
+mje= '<span style="coloh:red;font-weight:bold;">'+ name +' Debe esperar su turno.</span>']
 
+[h, if( isRango ),code:{
+[macro("RangoDos@lib:onTokenMove"):1] 
+}]
+
+[h,if( isSufiscientesPuntos == 0 || isInictiavaOken == 0): isNoMover=1]
 [h:tokens.denyMove = isNoMover]
-[r: mje]
+[r,if( isPC()): Mje]
