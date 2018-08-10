4d88d932e944f441b762bb1e1523d6d02777e050
Juan osudar <juan.0286@gmail.com>
2018-08-10 00:09:42 -0300

    arreglado cambio de frame acciones

--- Rol Tactico/Campaign/Sigfuiente.js
+++ Rol Tactico/Campaign/Sigfuiente.js
@@ -6,18 +6,16 @@
 [ if( roundNew > round ), CODE: {	
     [macro("ResetTokens@campaign"): 1]
     [macro("finAsalto@campaign"): 1]
-	[ if( tokName == "ACCION" ), CODE: {	
 	<!-- Envio el selector de acciones a los jugadores -->
-	[r: macroLink("Hacer Click!", "SelCambioAccion@Lib:asaltos")]
-	};{
-	<!-- Envio el selector de Cambio de Accion a los jugadores -->		
-	[r: macroLink("Hacer Click!", "SelAccion@Lib:asaltos")]
-	}]	
+	 [macro("linkSeleccionar@campaign"): 1]	 
 };{
+	<!-- Envio el selector de Cambio de Accion a los jugadores -->		
+[macro("linkCambios@campaign"): 1] 
 	[h: acc = getProperty("Accion", tok_id)]	
 	[h: val = getValIniciativeToken(tokName)]
 	[h: accionActual(tokName,val)]	
 }]
 
 
-[h, if( isPC(tok_id) ): goto(tok_id)]
+[h, if( isPC(tok_id) ): goto(tokName)]
+
