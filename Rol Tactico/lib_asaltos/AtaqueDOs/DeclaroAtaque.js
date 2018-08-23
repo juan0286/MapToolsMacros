e8b021c2c50b2b1f1d799ed8932da2c5d6367c8f
Juan Osudar <josudar@technisys.com>
2018-08-23 19:30:26 -0300

    Avance de DeclaroAtaquesDos casi andando

--- Rol Tactico/lib_asaltos/AtaqueDOs/DeclaroAtaque.js
+++ Rol Tactico/lib_asaltos/AtaqueDOs/DeclaroAtaque.js
@@ -9,8 +9,9 @@
 [h: penaGolpe = getStrProp(GolpeActual,"penaGolpe")]
 [h: estiloBO=""]
 
-[h,token(tokenAtk): imgList=getVisibleTokenNames()]
- 
+[h,token(tokenAtk): tokenList=getVisibleTokenNames()]
+[h: imgList = tokenList]
+
 [H: Num = listCount(imgList)]
  
 [h,COUNT(Num),CODE:
@@ -25,25 +26,25 @@
 
 [h: arrEstilos = listAppend('', estiloBo) ]
 
-[h, for(i,0,bo,10): arrEstilos = listAppend(arrEstilos, add("BO=",bo-i,"; BD=",bd+i,";") ) ]
-[h: arrEstilos = listAppend(arrEstilos, add("BO=",0,"; BD=",bd+bo,";") ) ]
+[h, for(i,0,bo,10): arrEstilos = listAppend(arrEstilos, add("BO=",bo-i,"; BD=",bdAgi+i,";") ) ]
+[h: arrEstilos = listAppend(arrEstilos, add("BO=",0,"; BD=",bdAgi+bo,";") ) ]
 
-[h: pause("Num","listArmas")]
+
 [h: input =input( 
-	"armasLbl|"+listArmas"+|Arma|LABEL",
+	"armasLbl|"+listArmas+"|Arma|LABEL",
 	"Target|"+imgList+"|Enemigo Objetivo|LIST|SELECT=0 ICON=TRUE ICONSIZE=30",
 	"boSeleccionada|"+ arrEstilos +"|Cuanto Bo Ataque / Defensa |LIST|SELECT=0 VALUE=STRING",
 	"penaGolpes|"+penaGolpe+"|Penaliza|LABEL|ICON=TRUE")]
 [h: abort(input)]
 
 [h: boFinal = getStrProp(boSeleccionada,"BO") + penaGolpe + bonoArma + boFija]
+[h: target = listGet(tokenList,Target)]
 
-[r: tokenDef = Target]
 [h: data = setStrProp("","arma",listArmas)]
 [h: data = setStrProp(data,"bofinal",boFinal)]
 [h: data = setStrProp(data,"tokenAtk",tokenAtk)]
-[h: data = setStrProp(data,"target",Target)]
-[h,token(tokenDef): jugadoresDef = getOwners()]
-[h, if( isPC(tokenDef) ): broadcast(macroLink("<color='red'>", "declaroDefensa@lib:asalto", 'none', data, ""), jugadoresDef) ; declaroDefensa(data) ]
+[h: data = setStrProp(data,"target",target)]
+[h,token(target): jugadoresDef = getOwners()]
+[h, if( isPC(target) ): broadcast(macroLink("<color='red'>", "declaroDefensa@lib:asalto", 'none', data, ""), jugadoresDef) ; declaroDefensa(data) ]
 
 
--- Rol Tactico/lib_asaltos/AtaqueDOs/declaroDefensa.js
+++ Rol Tactico/lib_asaltos/AtaqueDOs/declaroDefensa.js
@@ -1,41 +1,38 @@
 <!-- declaroDefensa -->
-[h: tokenDef =arg(0)]
-[switchToken(tokenAtk)]
+[h: data =arg(0)]
+[h: varsFromStrProp(arg(0))]
+[h: tokenDef = target]
+[h: pause("tokenDef")]
+[h: switchToken(tokenDef)]
+[h: pr = getProperty("GolpeActual",tokenDef)]
 [h: listArmas = "Espada, EspadaDos"]
 [h: bo = getStrProp(GolpeActual,"boActual")]
-[h: bd = getStrProp(GolpeActual,"bdActual")]
+[h: bd = getStrProp(GolpeActual,"bdAgiActual",tokenDef)]
+[h: bdEscudo = getStrProp(GolpeActual,"bonoEscudo")]
 [h: penaGolpe = getStrProp(GolpeActual,"penaGolpe")]
+
 [h: estiloBO=""]
 
-
-Declaro Defensa
-- bo parada
-- bd Agi
-- bd Escudo
-(bd fija)
-(sumo bonos mágicos si los hay)
-- (Modificador)Extra a desicion del máster (por cobertura, o alguna cosa, vaya a saber)
-// arma, boFinal, Target, tokenAtk
-[h: tokenDef = Target]
-[h: varsFromStrProp(arg(0))]
 [h,token(tokenAtk): image=getTokenImage()]
 
 
 <!-- Crear LIST de BO Disponible -->
-[h: arrEstilos = listAppend('', estiloBo) ]
-[h, for(i,0,bo,10): arrEstilos = listAppend(arrEstilos, add(bo-i) ]
-[h: arrEstilos = listAppend(arrEstilos, add(0) ) ]
+[h: arrEstilos = '' ]
+[h, for(i,0,bo,10): arrEstilos = listAppend(arrEstilos, add("BD=",i,"; BO=",bo-i,";") )) ]
+[h: arrEstilos = listAppend(arrEstilos, add("BD=",0,"; BO=",bo,";") ) ]
+
+
 
 <!-- Crear LIST de BD de AGI -->
 [h: arrAgiBd = '']
-[h, for(i,0,bdActual,5): arrAgiBd = listAppend(arrAgiBd, add("BD=",bd+i,"; BO=",bo-i,";") ) ]
+[h, for(i,0,bd,5): arrAgiBd = listAppend(arrAgiBd, add(bd-i) ) ]
 [h: arrAgiBd = listAppend(arrAgiBd, add(0) ) ]
 
 [h: input =input( 
 	"armasLbl|"+listArmas+"|Arma|LABEL",
 	"tokenAtkLbl|"+tokenAtk+" "+image+"|Atacante|LABEL|ICON=TRUE",	
 	"bdSeleccionada|"+ arrEstilos +"|Cuanto Bo usar para Defender?|LIST|SELECT=0 VALUE=STRING",
-	"bdAgiSel|"+ arrAgiBd +"|Cuanto Bo usar para Defender?|LIST|SELECT=0 VALUE=STRING",
-	"escudoCheck|1|Usar el Escudo?(+"+bdEscudo+" BD)|CHECK"
-	"TargetConcealed|0|Target has Concealment|CHECK")]
+	"bdAgiSel|"+ arrAgiBd +"|Cuanto AGI usar para Defender?|LIST|SELECT=0 VALUE=STRING",
+	"extraMod|0|Modificador Extra|TEXT",
+	"escudoCheck|1|Usar el Escudo?(+"+bdEscudo+" BD)|CHECK")]
 [h: abort(input)]
