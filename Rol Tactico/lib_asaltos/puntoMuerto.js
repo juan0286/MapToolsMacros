d6d06041f099e8d899265a9268c296034ae60035
Juan Osudar <josudar@technisys.com>
2018-09-24 14:06:37 -0300

    Solo agrego el boton de nuevo Asalto

---
+++ Rol Tactico/lib_asaltos/puntoMuerto.js
@@ -0,0 +1,23 @@
+[dialog("Seleccion de Acciones","width=500; height=500;"): {
+ <html>
+<head>
+  <link rel='stylesheet' type='text/css' href='myCSS@[r: getMacroLocation()]'></link>
+  <title>Punto Muerto</title>
+  <meta name="input" content="true">
+</head>
+<body>
+<table width=100%>
+<tr><th colspan=2>
+	<h1>Comienza Asalto : [r: getInitiativeRound()]</h1>
+</th></tr>
+<tr><th>Cargar Sortilegio</th></tr>
+<tr><th>Realizar Sortilegio</th></tr>
+<tr><th>Cargar Proyectil</th></tr>
+<tr><th>Disparar Proyectil</th></tr>
+<tr><th>Mov y Maniobra</th></tr>
+<tr><th>Combate Cuerpo a Cuerpo</th></tr>
+<tr><th>Movimiento Estatico</th></tr>
+</table>
+    </body>
+  </html>
+}]
