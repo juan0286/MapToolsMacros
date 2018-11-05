1f7958a05e41060a21d82b79942ccccd74d05b0e
Juan Osudar <josudar@technisys.com>
2018-11-05 18:11:19 -0300

    guardar accion solicita arco

--- Rol Tactico/lib_asaltos/Acciones/guardarAccion.js
+++ Rol Tactico/lib_asaltos/Acciones/guardarAccion.js
@@ -1,89 +1,104 @@
 <!--  guardarAccion -->
 
 [h: acc = arg(0)]
+[h, if(argCount() >1 ): switchToken(arg(1))]
 
 [h: tok_id = currentToken()]
+
+[h, if(argCount() >1 ): tok_id = arg(1)]
+[h: tokName= getName(tok_id)]
+
+[h, if(pausear()==1): pause("mm")]
 [h: mm = getMovMan(getName(tok_id))]
 [h: agi = getHoja("AGI",getName(tok_id))]
+
+
 
 [h: gm = isGM()]
 [h: owned = isOwner(getPlayerName(), tok_id)]
 [h, if(gm ||  owned): "";  assert(0, colorText("No tenes derecho a usar este Personaje.","red"),0)]
+[h, if(pausear()==1): pause("mm")]
 [h: pasos=3]
-[h: setInitiative(600 + mm + agi + bonoIniciativa)]	
-[h: desplazamiento= CalcMovimiento(tok_id)]
 [h: bonoIniciativa = getProperty("BonoIniciativa",tok_id)]
 [h, if(bonoIniciativa ==""): bonoIniciativa=0]
+[h, if(pausear()==1): pause("mm")]
 
 [h, switch(acc),code:
 case "cargar_sort": { 
-	[h: propsAccion =  json.set("{}", "accion", acc)]
+	[h: pre_cargarSort()]
+	[h: json =  json.set("{}", "accion", acc ,"desc",1)]
 	[h: setInitiative(1400 + mm + agi + bonoIniciativa)]		
-	[h: pasos=3]
 };
 case "lanzar_sort": { 
-	[h: propsAccion =  setStrProp("", "accion", acc)]
-	[h: propsAccion =  setStrProp(PropsAccion,"desc",1)]
-	[h: setInitiative(1200 + mm + agi + bonoIniciativa)]
-	[h: text = "Lanzara un sortilegio"]
+	[h: json =  json.set("{}", "accion", acc ,"desc",1)]
+	[h: setInitiative(1200 + mm + agi + bonoIniciativa)]	
 	[h: pasos=6]
 };
 case "cargar_proy": { 
-	[h: propsAccion =  setStrProp("", "accion", acc)]
-	[h: propsAccion =  setStrProp(PropsAccion,"desc",1)]
-	[h: setInitiative(1000 + mm + agi + bonoIniciativa)]
-	[h: pasos=3]		
+	
+	[h: if( json.get(brazo1,"tipoArma") == "Bow" ): b1 = 1 ; b1 = 0]
+	[h: if( json.get(brazo2,"tipoArma") == "Bow" ): b2 = 1 ; b2 = 0]
+
+	<!-- Se valida Que al menos una de las armas en mano, sea de proyectil-->
+	[h: ErrorMsg( b1 + b2,"Debe tener un arma Proyectil en mano")]
+
+	[h: json =  json.set("{}", "accion", acc ,"desc",1)]
+	[h: setInitiative(1000 + mm + agi + bonoIniciativa)]		
 };
 case "disparar_proy": { 
-	[h: propsAccion =  setStrProp("", "accion", acc)]
-	[h: propsAccion =  setStrProp(PropsAccion,"desc",1)]
+
+	[h: if( json.get(brazo1,"tipoArma") == "Bow" ): b1 = 1 ; b1 = 0]
+	[h: if( json.get(brazo2,"tipoArma") == "Bow" ): b2 = 1 ; b2 = 0]
+
+	<!-- Se valida Que al menos una de las armas en mano, sea de proyectil-->
+	[h: ErrorMsg( b1 + b2,"Debe tener un arma Proyectil en mano")]
+	
+	[h: json =  json.set("{}", "accion", acc ,"desc",1)]
 	[h: setInitiative(800 + mm + agi + bonoIniciativa)]	
-	[h: pasos=3]	
+	
 };
 case "mov_manio": { 
-	[h: propsAccion =  setStrProp("", "accion", acc)]
-	[h: propsAccion =  setStrProp(PropsAccion,"desc",1)]
-
-	<!-- Para mov y maniobra, el maximo es la mitad del mov basico -->		
-	[h: pasos = desplazamiento]
+	[h: json =  json.set("{}", "accion", acc ,"desc",1)]
+	[h: setInitiative(600 + mm + agi + bonoIniciativa)]	
 };
 case "ataque_cac": { 
-	[h: propsAccion =  setStrProp("", "accion", acc)]
-	[h: acc ,"bo",propsAccionPropsAccion,setStrProp(100, "bd", 50,"arma" ,"Espada","desc",1)]
-	[h: setInitiative(400 + mm + agi + bonoIn0iciativa)]	
-	[h: pasos=3]	
+	[h: json =  json.set("{}", "accion", acc ,"bo",100, "bd", 50,"arma" ,"Espada","desc",1)]
+	[h: setInitiative(400 + mm + agi + bonoIniciativa)]		
 };
 case "desplazamiento": { 
-	[h: propsAccion =  setStrProp("", "accion", acc)]
-	[h: propsAccion =  setStrProp(PropsAccion,"desc",1)]
+	[h: json =  json.set("{}", "accion", acc ,"desc",1)]
 	[h: setInitiative(200 + mm + agi + bonoIniciativa)]		
-	[h: pasos= CalcMovimiento(tok_id))]	
 };
 case "mov_estatico": { 
-	[h: propsAccion =  setStrProp("", "accion", acc)]
-	[h: propsAccion =  setStrProp(PropsAccion,"desc",1)]
+	[h: json =  json.set("{}", "accion", acc ,"desc",1)]
 	[h: setInitiative(mm + agi + bonoIniciativa)]	};
 default: { 
-	[h: propsAccion =  setStrProp("", "accion", acc)]
-	[h: propsAccion =  setStrProp(PropsAccion,"desc",1)]
+	[h: json =  json.set("{}", "accion", acc ,"desc",1)]
 	[h: setInitiative(mm + agi + bonoIniciativa)]
 	[h: pasos=0]
 }]
 
-[h: setStrProp("Accion",propsAccion,tok_id)]
-[h: setPropperty("ActualMove",3,tok_id)]
+[h: setProperty("Accion",json,tok_id)]
 
-[h: text = "La accion de " + getName(tok_id)+ " sera " + AccionToString(acc) ]
+[h: text = "La accion de " + tokName + " sera " + AccionToString(acc) ]
 
-[h: text = colorText(text,getProperty("color"))]
+
+
+[h: ga = getProperty("GolpeActual",tok_id)]
+[h: ga = setStrProp(ga,"pasos",pasos)]
+[h: setProperty("GolpeActual",ga,tok_id)]
+
+
+[h, if(isPC()): text = colorText(text,getProperty("color"))]
 
 [h: sortInitiative()]
 [r, if(isPC()), code:{
 	{text }
-	[h: showStatusFrame()]
+	[h: actionFrame()]
 };{
 	[h: broadcast(text, "gm")]	
 }] 
 [h: sortInitiative()]
 
-[h, if(isPC(tok_id)): broadcast(macroLink("<color='red'>", "showStatusFrame@lib:personajes", 'none', '', ""), getAllPlayerNames())]
+[h, if(isPC(tok_id)): broadcast(macroLink("<color='red'>", "actionFrame@lib:personajes", 'none', '', "selected"), getAllPlayerNames())]
+[h, if(isPC(tok_id)): broadcast(macroLink("<color='red'>", "puntoMuertoFrame@lib:personajes", 'none', '', "selected"), getAllPlayerNames())]
