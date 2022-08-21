a7a6bf930631e1503b0ae16c003c29881ae693af
Juan osudar <juan.0286@gmail.com>
2022-08-21 03:33:27 -0300

    Correciones en tknToIniciativaMisiles

---
+++ Rol GUerra/Codigo Macros/nuevo 4.ts
@@ -0,0 +1,50 @@
+QUE HACERES
+
+En los input de ataque, se deben setear RETAGUARDIA - FLANCO, y Modificador extra
+
+Terminar la macro de reiniciar Tokens
+
+Los overlays para todos los jugadores conectados
+
+MOdificadores por Formacion y tipo de unidad en los ataques
+
+MOdificadores por tipo de unidad contra tipo de unidad en los ataques
+
+Reglas especiales si hay un capitan en una unidad.
+
+Agregar tanto grupo A como B a la iniciativa en cada Fase de misiles. 
+
+Vision en el mapa, para poder esconde unidades en el BOsque
+
+PASOS COSTO:  Una variable que sume puntos, y la cual evita que la unidad de un paso.
+Ejemplo: La unidad cambio de encare, puede costarle 2 o 4 pasos.
+La unidad tiene un marcador de Fuego, pierde un paso.
+La unidad tiene un capitan, da un paso extra. (POner -1, y repetir hasta que sea 0)
+
+Crer una aura de 1 casillero, que sirva para conocer el Encare, se muestra siempre que se toque una ficha
+
+
+Cuadro de Calculo y tiradas de MORAL
+
+Variable Global, Grupo, que se intercambie en cada reinicio, y que las macros la consulten.
+
+Aura para columna y para Batalla. Columna debe tener un angulo menor.
+
+
+En el movimiento, el CUadro, deberia mostrar un calculo real del movimiento con las reglas
+
+
+Mostrar Turno en algun lado
+
+
+MOdificadores por estamina, o para que los puse?
+
+----> Miscellaneo
+
+Finales Carteles de Etapa
+
+Mejor imagen de control del footer
+
+En hojas, imprimir los cuadros importantes
+
+DUELO en pantalla con Overlay
