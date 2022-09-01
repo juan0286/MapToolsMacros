d0e52c0d6015038c369ea4cb82a958f1ff8346fa
Juan osudar <juan.0286@gmail.com>
2022-09-01 02:21:16 -0300

    Chequeo de moral, con cuadro
    Tambien agrego lib Asaltos de rol tactico. Falta agregar un boton al cambio de fase de Moral

--- Rol GUerra/Codigo Macros/nuevo 4.ts
+++ Rol GUerra/Codigo Macros/nuevo 4.ts
@@ -1,16 +1,8 @@
 QUE HACERES
-
-En los input de ataque, se deben setear RETAGUARDIA - FLANCO, y Modificador extra
-
-Terminar la macro de reiniciar Tokens
-
-Los overlays para todos los jugadores conectados
-
+ 
 MOdificadores por Formacion y tipo de unidad en los ataques
 
 MOdificadores por tipo de unidad contra tipo de unidad en los ataques
-
-Reglas especiales si hay un capitan en una unidad.
 
 Agregar tanto grupo A como B a la iniciativa en cada Fase de misiles. 
 
@@ -23,26 +15,45 @@ La unidad tiene un capitan, da un paso extra. (POner -1, y repetir hasta que sea
 
 Crer una aura de 1 casillero, que sirva para conocer el Encare, se muestra siempre que se toque una ficha
 
-
-Cuadro de Calculo y tiradas de MORAL
-
 Variable Global, Grupo, que se intercambie en cada reinicio, y que las macros la consulten.
 
 Aura para columna y para Batalla. Columna debe tener un angulo menor.
 
+Cuadro de Calculo y tiradas de MORAL
 
 En el movimiento, el CUadro, deberia mostrar un calculo real del movimiento con las reglas
 
+Los overlays para todos los jugadores conectados
 
-Mostrar Turno en algun lado
+Reglas especiales si hay un capitan en una unidad.
 
+Terminar la macro de reiniciar Tokens
 
-Se suma la estrategia del Capitan cuando se calcula la BD
-
-
-se pùede usar parry contra una unidad atacante que ataca por el flanco
+Mostrar Turno en algun lado  Grupo A o Grupo B
 
 MOdificadores por estamina, o para que los puse?
+
+
+
+Se suma la estrategia del Capitan cuando se calcula la BD? si, en la BO
+
+
+se pùede usar parry contra una unidad atacante que ataca por el flanco?
+
+
+
+--
+
+SISTEMA FORMACION GENERAL
+Para guardar un movimiento de  una unidad general, se deben mover junto a su Ficha auxiliar.
+Si se mueve una sola, se debe anular el movimiento.
+
+Si mando a atacar a la ficha auxiliar, se debe utilizar como tkn a la ficha original.
+
+Cuando se ejecuta moverUnidadxDia, si es general, deberia mover ambas fichas, la origen y la auxiliar, hasta que una de las dos encuentre una colision o ambas lleguen a destino
+
+
+
 
 ----> Miscellaneo
 
